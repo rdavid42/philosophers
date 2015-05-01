8863a742c70b2456a16a2809239d29ce9dab9dc7
Ryd <rdavid@student.42.fr>
2015-05-01 18:54:07 +0200

    moar norme

--- src/draw_philosophers.c
+++ src/draw_philosophers.c
@@ -25,9 +25,13 @@ void				draw_philosophers(t_core *c)
 	i = -1;
 	while (++i < PN)
 	{
-		draw_text(x - c->g.p_radius, y + c->g.p_radius + c->g.p_padding, state[c->p[i].state], F1);
-		draw_text(x - c->g.p_radius, y + c->g.p_radius + c->g.p_padding + 20, "life: ", F1);
-		draw_text(x - c->g.p_radius + 40, y + c->g.p_radius + c->g.p_padding + 20, itoa(c->p[i].life), F1);
+		draw_text(x - c->g.p_radius, y + c->g.p_radius + c->g.p_padding,
+					state[c->p[i].state], F1);
+		draw_text(x - c->g.p_radius, y + c->g.p_radius + c->g.p_padding + 20,
+					"life: ", F1);
+		draw_text(x - c->g.p_radius + 40,
+					y + c->g.p_radius + c->g.p_padding + 20,
+					itoa(c->p[i].life), F1);
 		draw_circle(x, y, c->g.p_radius, 20);
 		x += c->g.p_radius * 2 + c->g.p_padding;
 	}
